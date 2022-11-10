27cf6ffd1bce7c3207372c3423f80d23460fffc6
Sumeet Debnath <sumeetdeb100@gmail.com>
2022-11-10 17:18:12 +0530

    updated Sponsor page

--- pages/Sponsor.js
+++ pages/Sponsor.js
@@ -6,6 +6,56 @@ const Sponsor = () => {
   return (
     <div className={`py-20 ${gradients.homePage} text-black`}>
 
+<div className='mt-6 p-2'>
+        <h2 className='text-center text-2xl md:text-3xl font-extrabold'>
+            <span className='border-b-4 border-orange-400 pb-2 px-2 md:px-8'>
+            Welcome Message
+            </span>
+        </h2>
+    <div className='p-2 pt-8 lg:text-lg lg:px-16'>
+    <p className='text-justify'>
+    Finance and Organizing Committee MAC 2023 requests support for the upcoming 1st International
+Conference on Microwave, Antenna and Communication (IEEE MAC 2023) that is scheduled
+during March 24-26, 2023 at the Department of Electronics & Communication Engineering, Motilal
+Nehru National Institute of Technology Allahabad, Prayagraj, U.P, India. Support from our commercial
+sponsors will instrumental in maintaining the quality of the conference, and we would greatly appreciate
+any financial commitment from the sponsor partners for this Conference.
+        </p>
+        <br/>
+        <p className='text-justify'>
+        The Organizing Committee will invite research scientists and enterprises to present the different
+perspectives and applications in this conference. This conference is not only a spectacular meeting to
+share information and knowledge about microwave, antenna and emerging trends in communication
+system, but also a pivotal help to build an effective platform for communication and cooperation among
+the academic researcher in world wide. The preliminary information on the 1st International
+Conference on Microwave, Antenna and Communication (MAC 2023) are available on the website
+<a href='http://www.ieeemac.org' target='_blank' rel="noreferrer" className='text-blue-800 font-semibold'> (http://www.ieeemac.org)</a>. 
+        </p>
+        <br/>
+        <p className='text-justify'>
+        This Conference will be under the supervision and control of both the International/National Scientific
+Committee composed of the following researcher’s like Prof. Bharat Rawal, Benedict college
+Columbia, USA, Prof. K.P. Ray, DRDO, India, Prof. Andrea Massa, University of Trento, Italy, Prof.
+Zhi Ning Chen, ARTIC, NUS Singapore, Prof. Abhay Karandikar, IIT Kanpur, India, Dr. Debdeep
+Sarkar, IISC Banglore, India, Dr. Chandrakanta Kumar, ISRO, India, Dr. Goutam Chattopadhyay, JPL,
+NASA, USA and the Local Organizing Committee. The scientific program will consist of a combination
+of invited presentations chosen through review of the current scientific literature and Oral and Poster
+presentations selected from the submitted abstracts. The conference consists of various sessions
+including keynote, plenary and parallel sessions. Each session will be addressed by outstanding experts
+who will highlight recent advances in various facets of microwave communication, antenna design and
+communication system design with focus on emerging trends. It is our privilege to promote young
+researchers and students from the all over the world.
+        </p>
+        <br/>
+        <p className='text-justify font-semibold'>
+        “We would like to offer an opportunity to put your company’s brand in front of key academic
+professionals, who have a vested interest in your products, by becoming a sponsor. Sponsorship at the
+MAC 2023 will show our attendees your commitment to building industry academic relationships. A
+variety of sponsorship opportunities are available so you may choose your level of support and
+exposure.” 
+        </p>
+    </div>
+    </div>
         <div className='mt-6 p-2'>
         <h2 className='text-center text-2xl md:text-3xl font-extrabold'>
             <span className='border-b-4 border-orange-400 pb-2 px-2 md:px-8'>
@@ -13,7 +63,7 @@ const Sponsor = () => {
             </span>
         </h2>
     <div className='p-2 pt-8 lg:text-lg lg:px-16'>
-    <p>
+    <p className='text-justify'>
         The conference is getting exposure worldwide to personals associated with microwave, antenna and
 recent trends in communication, which will definitely provide a lot exposure to sponsors and their
 identification to R&D in relevant field. <span className='font-bold'>
@@ -22,11 +72,11 @@ conference material, banners and in the satchel.
 </span>
         </p>
         <br/>
-        <p>
+        <p className='text-justify'>
         On behalf of the executive committee of MAC 2023, it is our great pleasure to invite you to be a valued
 sponsor at the event. MAC offers multiple levels of sponsorship and exhibitor opportunities to promote
 your organization to this influential audience. We would like you to sponsor this event as a
-Diamond/Platinum/Gold/Silver/Bronze sponsor.
+Diamond/Platinum/Gold/Silver sponsor.
         </p>
         <br/>
         <h3>Looking forward to see you in the MAC 2023, at MNNITA, Prayagraj (U.P).</h3>
@@ -34,17 +84,23 @@ Diamond/Platinum/Gold/Silver/Bronze sponsor.
     </div>
     </div>
 
+<div className='text-center my-6'>
+    <button className='btn bg-cyan-600'>
+    <a href='/SponsorshipBenefits.pdf' download={true}>
+    Download Sponsorship Details
+    </a></button>
+</div>
         <div className='mt-6 p-2'>
             <h1 className='font-extrabold text-2xl md:text-3xl text-center'>
                 <span className=' border-b-4 border-orange-500 px-0 md:px-8 pb-2'>
-                Level Of <span className='text-orange-500'>Sponsorships</span>
+                Level Of <span className='text-cyan-600'>Sponsorships</span>
                 </span>
             </h1>
 
-       <div className='lg:px-16'>
+       <div className='lg:px-16 flex flex-col items-center'>
        <div className={`px-2 card shadow-xl mt-8 pt-4 ${cool.coolHover}`}>
-                <h2 className='text-xl lg:text-2xl font-bold p-2'><span className='text-red-600'>
-                Signature
+                <h2 className='text-xl lg:text-2xl font-bold p-2'><span className='text-cyan-600'>
+                Spotlight
                 </span> sponsorship: <span className='underline'>INR 3.0 Lakh</span>
                 </h2>
                 <ul className='list-disc pl-4 lg:px-8 lg:pb-6 py-4 text-md lg:text-lg'>
@@ -69,8 +125,9 @@ Diamond/Platinum/Gold/Silver/Bronze sponsor.
                 </ul>
             </div>
 
-            <div className={`px-2 card shadow-xl mt-8 pt-4 ${cool.coolHover}`}>
-                <h2 className='text-xl lg:text-2xl font-bold p-2'><span className='text-red-600'>
+           <div className='w-full flex flex-col lg:flex-row gap-8 justify-between'>
+           <div className={`px-2 card shadow-xl mt-8 pt-4 ${cool.coolHover}`}>
+                <h2 className='text-xl lg:text-2xl font-bold p-2'><span className='text-cyan-600'>
                 Diamond
                 </span> sponsorship: <span className='underline'>INR 2.0 Lakh</span></h2>
                 <ul className='list-disc pl-4 lg:px-8 lg:pb-6 py-4 text-md lg:text-lg'>
@@ -96,7 +153,7 @@ Diamond/Platinum/Gold/Silver/Bronze sponsor.
             </div>
 
             <div className={`px-2 card shadow-xl mt-8 pt-4 ${cool.coolHover}`}>
-                <h2 className='text-xl lg:text-2xl font-bold p-2'><span className='text-red-600'>
+                <h2 className='text-xl lg:text-2xl font-bold p-2'><span className='text-cyan-600'>
                 Platinum
                 </span> sponsorship: <span className='underline'>INR 1.5 Lakh</span></h2>
                 <ul className='list-disc pl-4 lg:px-8 lg:pb-6 py-4 text-md lg:text-lg'>
@@ -120,10 +177,12 @@ Diamond/Platinum/Gold/Silver/Bronze sponsor.
                     </li>
                 </ul>
             </div>
+           </div>
 
-            <div className={`px-2 card shadow-xl mt-8 pt-4 ${cool.coolHover}`}>
+            <div className='w-full flex flex-col lg:flex-row gap-8 justify-between'>
+            <div className={`px-2 card lg:w-[50%] shadow-xl mt-8 pt-4 ${cool.coolHover}`}>
                 <h2 className='text-xl lg:text-2xl font-bold p-2'>
-                <span className='text-red-600'>Gold</span> sponsorship: <span className='underline'>INR 1.0 Lakh</span></h2>
+                <span className='text-cyan-600'>Gold</span> sponsorship: <span className='underline'>INR 1.0 Lakh</span></h2>
                 <ul className='list-disc pl-4 lg:px-8 lg:pb-6 py-4 text-md lg:text-lg'>
                     <li >
                     5 Complimentary delegate passes
@@ -143,9 +202,9 @@ Diamond/Platinum/Gold/Silver/Bronze sponsor.
                 </ul>
             </div>
 
-            <div className={`px-2 card shadow-xl mt-8 pt-4 ${cool.coolHover}`}>
+            <div className={`px-2 card lg:w-[50%] shadow-xl mt-8 pt-4 ${cool.coolHover}`}>
                 <h2 className='text-xl lg:text-2xl font-bold p-2'>
-                <span className='text-red-600'>Silver</span> sponsorship: <span className='underline'>INR 50 thousand</span></h2>
+                <span className='text-cyan-600'>Silver</span> sponsorship: <span className='underline'>INR 50 thousand</span></h2>
                 <ul className='list-disc pl-4 lg:px-8 lg:pb-6 py-4 text-md lg:text-lg'>
                     <li >
                     2 Complimentary delegate passes
@@ -160,6 +219,7 @@ Diamond/Platinum/Gold/Silver/Bronze sponsor.
                     Display of logo on all printed material and website
                     </li>
                 </ul>
+            </div>
             </div>
        </div>
         </div>
