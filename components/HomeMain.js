e5a059759be843f62c9bbec1ac25e7e07d2021fa
Ankit Kumar <0404ankitkumar@gmail.com>
2023-01-25 01:30:32 +0530

    updated popup

--- components/HomeMain.js
+++ components/HomeMain.js
@@ -21,13 +21,13 @@ const HomeMain = () => {
     router.push(dest);
   };
 
-  // useEffect(()=>{
-  //   Swal.fire({
-  //     icon: 'warning',
-  //     title: 'Important Notice',
-  //     html: 'The first phase of acceptance notification has been send to those authors who have submitted their papers on or before 22 Nov, 2022. The acceptance notification for the remaining papers will be notified soon.',
-  //   })
-  // },[])
+  useEffect(()=>{
+    Swal.fire({
+      icon: 'warning',
+      title: 'Important Notice',
+      html: 'The first phase of acceptance notification has been send to those authors who have submitted their papers on or before 22 Nov, 2022. The acceptance notification for the remaining papers will be notified soon.',
+    })
+  },[])
 
   return (
  <>
