db8b747678f0df339a2b09f96de7e69ee4c72e98
Ankit Kumar <0404ankitkumar@gmail.com>
2023-03-22 21:04:31 +0530

    diamond sponsers added

--- components/HomeMain.js
+++ components/HomeMain.js
@@ -184,12 +184,12 @@ const HomeMain = () => {
             </h1>
             {/* <h1 className="relative title-font mb-4 font-bold border-b-4 pb-4 text-2xl md:text-4xl lg:pr-24 border-cyan-600 w-full md:w-[60%]">Technical Co Sponsors
       </h1> */}
-            {/* <div className={`mt-10 bg-stone-100 shadow-inner rounded-lg flex flex-col justify-center items-center ${cool.coolHover}`}>
+            <div className={`mt-10 bg-stone-100 shadow-inner rounded-lg flex flex-col justify-center items-center ${cool.coolHover}`}>
                 <h1 className='text-center font-extrabold text-2xl tracking-wide leading-relaxed md:text-3xl pt-2 text-cyan-600'>Diamond</h1>
                 <img src="/numeregion.webp" alt="logo" className='w-96 text-center px-4 pb-8 md:pb-12 pt-2 mt-4 md:pt-6 ease-in-out transition transform over:-translate-y-1 hover:scale-110 md:hover:scale-125 duration-700 delay-75' />
                 <p className='px-2 text-center text-xl md:text-2xl font-semibold pb-6'>NUMEREGION's TaraNG</p>
 
-            </div> */}
+            </div>
             <div className={`mt-8 bg-stone-100 shadow-inner rounded-lg flex flex-col justify-center items-center ${cool.coolHover}`}>
                 <h1 className='text-center font-extrabold text-2xl tracking-wide leading-relaxed md:text-3xl pt-2 text-cyan-600'>Platinum</h1>
                 <img src="/serb.webp" alt="logo" className='text-center px-4 pb-8 md:pb-12 pt-2 md:pt-6 ease-in-out transition transform over:-translate-y-1 hover:scale-110 md:hover:scale-125 duration-700 delay-75' />
